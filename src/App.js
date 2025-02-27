0af288dd2ad7935893345088507ff93d212033fa
yaoyaoxie <97951645+yaoyaoxie@users.noreply.github.com>
2025-02-27 17:56:46 +0800

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TimeTracker from './TimeTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <TimeTracker />
+    </div>
+  );
+}
+
+export default App;
